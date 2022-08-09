26d72cdccbb369e52ede1366103bc9bb8f183507
LukeNK <lukenguyenkhanh@gmail.com>
2022-08-09 09:55:47 -0700

    Additional edit, I go

---
+++ README.md
@@ -0,0 +1,4 @@
+# Todo list
+- [ ] Fix navbar
+- [ ] Fix horizontal scroll and overflow
+- [ ] Add more template 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Bayer Free State Government' });
 });
+
+router.get('/government', (req, res, next) => {
+
+})
+
+router.get('/MP/:id', (req, res, next) => {
+
+})
+
+router.get('/templates/:name', (req, res, next) => {
+  res.render(`templates/${req.params.name}`, {title: 'Bayer Free State templates'})
+})
 
 module.exports = router;
