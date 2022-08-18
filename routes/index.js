c4a41ff2417c9511e1e54883952a18d559378dc4
LukeNK <lukenguyenkhanh@gmail.com>
2022-08-17 19:53:18 -0700

    Baisc new stuff for election and government Seriously I need to check grammar

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Bayer Free State Government' });
+  res.render('index', { title: 'Bayer Free State' });
 });
 
+// Need to fix these route so it can accept multiple sub-route
+
 router.get('/government', (req, res, next) => {
+  res.render(`government/index`, {title: 'Bayer Free State Government'})
+});
+router.get('/government/:name', (req, res, next) => {
+  res.render(`government/${req.params.name}`, {title: req.params.name})
+});
 
-})
-
-router.get('/MP/:id', (req, res, next) => {
-
-})
+router.get('/elections', (req, res, next) => {
+  res.render(`elections/index`, {title: 'Bayer Free State Elections'})
+});
+router.get('/elections/:name', (req, res, next) => {
+  res.render(`elections/${req.params.name}`, {title: req.params.name})
+});
 
 router.get('/templates/:name', (req, res, next) => {
   res.render(`templates/${req.params.name}`, {title: 'Bayer Free State templates'})
 })
 
-module.exports = router;
+module.exports = router;``
